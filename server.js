dcc25057658a5c76e08427bb26fb5ce2c8c5040e
sion99 <kmicety1@gmail.com>
2023-04-13 20:28:46 +0900

    server.js

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}))
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+MongoClient.connect('mongodb+srv://shinsion:dr0907@cluster0.p1jxo64.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
+	if (에러) return console.log(에러)
+
+	db = client.db('todoapp');
+
+	db.post.insertOne( {이름 : 'John', 나이 : 20} , function(에러, 결과){
+		console.log('저장완료');
+	});
+
+	app.listen(8080, function() {
+	  console.log('listening on 8080')
+	});
+  });
+
 
 app.listen(8080, function(){
 	console.log('listening on port 8080');
 });
 
-app.get('/pet', function(요청, 응답){
-	응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
-});
-
-app.get('/beauty', function(req, res){
-	res.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
-})
-
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
-})
+});
+
+app.get('/write', function(req, res){
+	res.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function(req, res){
+	res.send('전송완료');
+	console.log(req.body);
+});
